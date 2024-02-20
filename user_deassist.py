abeeb19a5a7cda4bf063181f913bce8d0eaaab76
Jesse Shelley <realjesseshelley@gmail.com>
2024-02-19 17:47:12 -0800

    args

--- user_deassist.py
+++ user_deassist.py
@@ -1,3 +1,4 @@
+import argparse
 import winreg
 
 
@@ -7,18 +8,21 @@ class UserAssist:
         self.key_path = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced"
 
     def toggle(self, enabled: bool) -> None:
-        if enabled:
-            key = winreg.OpenKey(self.hive, self.key_path)
-            winreg.SetValueEx(key, "Start_TrackProgs", 0, winreg.REG_DWORD, 1)
-            winreg.SetValueEx(key, "Start_TrackEnabled", 0, winreg.REG_DWORD, 1)
-            winreg.CloseKey(key)
-            print(f"[+] UserAssist Logging Enabled")
-        else:
-            key = winreg.OpenKey(self.hive, self.key_path)
-            winreg.SetValueEx(key, "Start_TrackProgs", 0, winreg.REG_DWORD, 0)
-            winreg.SetValueEx(key, "Start_TrackEnabled", 0, winreg.REG_DWORD, 0)
-            winreg.CloseKey(key)
-            print(f"[+] UserAssist Logging Disabled")
+        key = winreg.OpenKey(self.hive, self.key_path, 0, winreg.KEY_SET_VALUE)
+
+        try:
+            if enabled:
+                winreg.SetValueEx(key, "Start_TrackProgs", 0, winreg.REG_DWORD, 1)
+                winreg.SetValueEx(key, "Start_TrackEnabled", 0, winreg.REG_DWORD, 1)
+                print(f"[+] UserAssist Logging Enabled")
+            else:
+                winreg.SetValueEx(key, "Start_TrackProgs", 0, winreg.REG_DWORD, 0)
+                winreg.SetValueEx(key, "Start_TrackEnabled", 0, winreg.REG_DWORD, 0)
+                print(f"[+] UserAssist Logging Disabled")
+        except Exception as e:
+            print(e)
+
+        winreg.CloseKey(key)
 
     def enum_value(self):
         key = winreg.OpenKey(self.hive, self.key_path)
@@ -26,44 +30,53 @@ class UserAssist:
 
         try:
             for i in range(0, total_values):
-                found = False
                 value_name = winreg.EnumValue(key, i)[0]
                 if value_name == "Start_TrackProgs":
-                    found = True
                     value = winreg.EnumValue(key, i)[1]
                     if value == 1:
                         print(f"[+] Start_TrackProgs is enabled.")
                     else:
                         print(f"[+] Start_TrackProgs is disabled.")
-                elif found is False and i == total_values - 1:
-                    print(f"[!] Start_TrackTrackProgs value not found.")
-
         except Exception as e:
             print(e)
 
         try:
             for i in range(0, total_values):
-                found = False
                 value_name = winreg.EnumValue(key, i)[0]
                 if value_name == "Start_TrackEnabled":
-                    found = True
                     value = winreg.EnumValue(key, i)[1]
                     if value == 1:
                         print(f"[+] Start_TrackEnabled is enabled.")
                     else:
                         print(f"[+] Start_TrackEnabled is disabled.")
-                elif found is False and i == total_values - 1:
-                    print(f"[!] Start_TrackTrackEnabled value not found.")
-            winreg.CloseKey(key)
-
         except Exception as e:
             print(e)
 
+        print(f"[!] If no output, the keys do not exist.")
+        winreg.CloseKey(key)
+
 
 def main():
+    parser = argparse.ArgumentParser(description="Enable/Disable UserAssist Program Logging")
+    parser.add_argument('--disable', required=False, dest="disable", action="store_true")
+    parser.add_argument('--enable', required=False, dest="enable", action="store_true")
+    parser.add_argument('--enum', required=False, dest="enum", default=False, action="store_true")
+
+    args = parser.parse_args()
+
     ua = UserAssist()
-    ua.enum_value()
-    ua.toggle(False)
+
+    if args.disable:
+        ua.toggle(False)
+
+    if args.enable:
+        ua.toggle(True)
+
+    if args.enum:
+        print(f"[!] Warning: Running enum will create a registry access event.")
+        response = input("Would you like to continue? (y/n)")
+        if response.lower() == "y":
+            ua.enum_value()
 
 
 if __name__ == '__main__':
